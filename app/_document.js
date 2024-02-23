80eebed916d6dc627562b43ae6ecc2194b5fbdd6
wasupon <wasupon.mail@gmail.com>
2024-02-23 14:45:30 +0900

    Fix: preload font in next js custom document

--- app/_document.js
+++ app/_document.js
@@ -1,23 +1,35 @@
-<Head>
-  <link
-    rel="preload"
-    href="../public/fonts/RobotoMono-VariableFont_wght.woff2"
-    as="font"
-    type="font/woff2"
-    crossOrigin="anonymous"
-  />
-  <link
-    rel="preload"
-    href="../public/fonts/Outfit-VariableFont_wght.woff2"
-    as="font"
-    type="font/woff2"
-    crossOrigin="anonymous"
-  />
-  <link
-    rel="preload"
-    href="../public/fonts/Manrope-VariableFont_wght.woff2"
-    as="font"
-    type="font/woff2"
-    crossOrigin="anonymous"
-  />
-</Head>;
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="../public/fonts/RobotoMono-VariableFont_wght.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="../public/fonts/Outfit-VariableFont_wght.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="../public/fonts/Manrope-VariableFont_wght.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
